e8e5a63df72c42f67bceb3203d7e9dde8c5c615a
sohilhasnani <sohilhasnani@gmail.com>
2026-09-13 14:01:04 +0530

    small change

--- src/main/java/com/example/cashbookexample/HelloWorldController.java
+++ src/main/java/com/example/cashbookexample/HelloWorldController.java
@@ -7,7 +7,7 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World. Welcome to the cashbook";
     }
 	
 	
