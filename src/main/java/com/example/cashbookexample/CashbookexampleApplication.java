9cebd18a8d739b2d5e814f93ffeff31df8031206
sohilhasnani <sohilhasnani@gmail.com>
2026-09-13 12:01:17 +0530

    initial commit

---
+++ src/main/java/com/example/cashbookexample/CashbookexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashbookexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbookexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbookexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cashbookexample/CashbookexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashbookexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbookexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
